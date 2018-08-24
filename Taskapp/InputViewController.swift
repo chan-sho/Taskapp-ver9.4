219d59b9ee3653ea3bbe8ff8a538b2d081d9d7ba
高野翔 <ShoTakano@43x232x203x168.ap43.ftth.ucom.ne.jp>
2018-08-24 22:27:28 +0900

    category追加

--- Taskapp/InputViewController.swift
+++ Taskapp/InputViewController.swift
@@ -12,6 +12,7 @@ import UserNotifications
 
 class InputViewController: UIViewController {
 
+    @IBOutlet weak var categoryTextField: UITextField!
     @IBOutlet weak var titleTextField: UITextField!
     @IBOutlet weak var contentsTextView: UITextView!
     @IBOutlet weak var datePicker: UIDatePicker!
@@ -26,6 +27,7 @@ class InputViewController: UIViewController {
         let tapGesture: UITapGestureRecognizer = UITapGestureRecognizer(target:self, action:#selector(dismissKeyboard))
         self.view.addGestureRecognizer(tapGesture)
         
+        categoryTextField.text = task.category
         titleTextField.text = task.title
         contentsTextView.text = task.contents
         datePicker.date = task.date
@@ -38,6 +40,7 @@ class InputViewController: UIViewController {
     
     override func viewWillDisappear(_ animated: Bool) {
         try! realm.write {
+            self.task.category = self.categoryTextField.text!
             self.task.title = self.titleTextField.text!
             self.task.contents = self.contentsTextView.text
             self.task.date = self.datePicker.date
--- Taskapp/Task.swift
+++ Taskapp/Task.swift
@@ -14,6 +14,9 @@ class Task: Object {
     // 管理用 ID。プライマリーキー
     @objc dynamic var id = 0
     
+    // カテゴリー
+    @objc dynamic var category = ""
+    
     // タイトル
     @objc dynamic var title = ""
     
